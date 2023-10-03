224a8cca21b9a7104fb0263b4dc5e086fba9db00
Cairo <cairofelipedev@gmail.com>
2023-10-03 00:03:54 -0300

    Add swiper slick and structure app curses

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,8 +2,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="pt-br">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;900&display=swap" rel="stylesheet" />
+        <title>Teste</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
