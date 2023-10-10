50c38fc2e9fc2c355e036b247a578ccedf9f45d9
Cairo <cairodev@icloud.com>
2023-10-10 12:55:26 -0300

    Stripe working

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,19 @@
 import Head from 'next/head';
+import { GetStaticProps } from 'next';
 
 import SwiperSlider from '@/components/Swiper';
 import Products from '@/components/Products';
+import { stripe } from '../services/stripe'
 
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number
+  }
+}
 
-export default function Home() {
+export default function Home({ product }: HomeProps) {
+
   return (
     <>
       <Head>
@@ -12,6 +21,24 @@ export default function Home() {
       </Head>
       <SwiperSlider />
       <Products />
+      <h1>{product.amount}</h1>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1NzgmPD5nW942IVIBmTI72vN', {
+    expand: ['product']
+  })
+
+  const product = {
+    priceId: price.id,
+    amount: (price.unit_amount / 100),
+  };
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24,
+  }
 }
