0da8712249c6f6c6892676bcce3ee6ed2f0ffd83
Cairo <cairofelipedev@gmail.com>
2023-10-17 22:53:33 -0300

    Login provider google

--- components/Sign.tsx
+++ components/Sign.tsx
@@ -1,18 +1,21 @@
+import { signIn, useSession } from "next-auth/react"
+
 export default function Sign() {
-  const isUserLoggedIn = false
-  return isUserLoggedIn ? (
+  const { data: session } = useSession()
+  console.log(session);
+  return session ? (
     <>
       <a href="/about">
         <button className="group relative h-12 w-48 overflow-hidden rounded-2xl bg-pink-500 text-lg font-bold text-white">
-          Luciana
+          {session.user?.name}
           <div className="absolute inset-0 h-full w-full scale-0 rounded-2xl transition-all duration-300 group-hover:scale-100 group-hover:bg-white/30"></div>
         </button>
       </a>
     </>
   ) : (
-    <a href="/about">
-      <button className="group relative h-12 w-48 overflow-hidden rounded-2xl bg-red-500 text-lg font-bold text-white">
-        Assine Agora
+    <a href="#">
+      <button className="group relative h-12 w-48 overflow-hidden rounded-2xl bg-red-500 text-lg font-bold text-white" onClick={() => signIn('google')}>
+        Faça o login agora
         <div className="absolute inset-0 h-full w-full scale-0 rounded-2xl transition-all duration-300 group-hover:scale-100 group-hover:bg-white/30"></div>
       </button>
     </a>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,13 @@ import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 import NavBar from '@/components/NavBar';
 import type { AppProps } from 'next/app'
+import { SessionProvider } from 'next-auth/react';
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <>
+    <SessionProvider session={session}>
       <NavBar />
       <Component {...pageProps} />
-    </>
+    </SessionProvider>
   )
 }
