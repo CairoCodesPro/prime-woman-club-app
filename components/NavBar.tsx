a12d7b4edd8a6627c4f69f981df681ec6b49a85f
Cairo <cairodev@icloud.com>
2023-10-30 17:57:02 -0300

    Add imgs

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Sign from '@/components/Sign';
 export default function NavBar() {
   return (
@@ -5,7 +6,7 @@ export default function NavBar() {
       <div className="w-full flex items-center justify-between border-b shadow-sm py-8 px-10 mb-6">
         <div>
           <a className="text-2xl font-bold font-heading" href="#">
-            Prime <span className="text-pink-500">Woman</span> Club
+          <Image src="/logotext.png" alt="Vercel Logo" width={500} height={50} />
           </a>
         </div>
         <div>
